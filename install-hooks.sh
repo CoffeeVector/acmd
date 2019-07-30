f0c2af40660e4e841257836d90fad644fa7b98e6
Kevin Zheng <coffeevector@gmail.com>
2019-07-29 23:33:55 -0700

    Removed white space
    Actually Closes #3

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d .git/ ]; then 
+if [ -d .git/ ]; then
     if [ -f .git/hooks/post-merge ] && ! [ -z $(diff .git/hooks/post-merge hooks/post-merge) ]; then
         echo "Move your post-merge to something with the extension post-merge, e.g. something.post-merge. If this is an old version generated from this script, delete it."
         exit 1
