f3bc28fa2c741e2c59449e9e90adafdfe82d22e6
pranav <pranavbhusari2013@gmail.com>
2019-07-29 22:25:49 -0700

    added the `apod` command, prints out the astronomy picture of the day

--- command.py
+++ command.py
@@ -1,4 +1,6 @@
-import requests 
+import requests
+
+
 class WeatherCommand:
     def pertains(self, command, raw_command, spell_command):
         return 'weather' in command or 'weather' in spell_command
@@ -12,14 +14,17 @@ class WeatherCommand:
         elif "f" in command or "fahrenheit" in command or "f" in spell_command or "fahrenheit" in spell_command:
             print(requests.get('https://wttr.in/?u&0').text)
         else:
-            print(requests.get('https://wttr.in/?m&0').text) # ha metric for the win
+            print(requests.get('https://wttr.in/?m&0').text)  # ha metric for the win
+
 
 import os
 import re
+
+
 class MathCommand:
     def pertains(self, command, raw_command, spell_command):
         return 'calculate' in command or 'calculate' in spell_command
-    
+
     def run(self, command, raw_command, spell_command):
         if os.popen('command -v bc') == "":
             print("Sorry, I'm too stupid to do math (install bc on your unix system).")
@@ -32,23 +37,25 @@ class MathCommand:
         if not printed:
             print("I don't know what to calculate...")
 
+
 import urllib
 import subprocess
-import sys
 
 from lxml import html
+
+
 class GoogleCommand:
     def pertains(self, command, raw_command, spell_command):
         keywords = ['google', 'search']
         return any([i in command for i in keywords]) or any([i in spell_command for i in keywords])
 
-    def run(self, command, raw_command, spell_command): 
+    def run(self, command, raw_command, spell_command):
         try:
-            index_of_google = command.index('google') 
+            index_of_google = command.index('google')
         except:
             index_of_google = len(command)
         try:
-            index_of_search = command.index('search') 
+            index_of_search = command.index('search')
         except:
             index_of_search = len(command)
         index_of_google = index_of_google if (index_of_google < index_of_search) else index_of_search
@@ -64,11 +71,12 @@ class GoogleCommand:
         links = links + [url]
         subprocess.run(['google-chrome', '--new-window'] + links[:5])
 
+
 class TodoCommand:
     def pertains(self, command, raw_command, spell_command):
         keywords = ['todo', 'work']
         return any([i in command for i in keywords]) or any([i in spell_command for i in keywords])
-    
+
     def run(self, command, raw_command, spell_command):
         if os.popen('command -v shuf').read() == "":
             print("Sorry, I'm too stupid to do random things (install shuf on your unix system).")
@@ -76,4 +84,24 @@ class TodoCommand:
         print(filename)
         os.popen('st -e vim ~/.todo/' + filename)
 
-available_commands = [WeatherCommand(), MathCommand(), GoogleCommand(), TodoCommand()]
+from imageDisplayer import show_image
+from io import BytesIO
+from PIL import Image
+
+class ApodCommand:
+    def pertains(self, command, raw_command, spell_command):
+        keywords = ['apod', 'astronomy']
+        return any([i in command for i in keywords]) or any([i in spell_command for i in keywords])\
+
+    def run(self, command, raw_command, spell_command):
+        url = requests.get("https://api.nasa.gov/planetary/apod?date=2018-08-04&hd=True&api_key=DEMO_KEY").json()["hdurl"]
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+
+        # show_image is a command that shows a image in the terminal given a PIL Image object
+        show_image(img)
+
+
+
+
+available_commands = [WeatherCommand(), MathCommand(), GoogleCommand(), TodoCommand(), ApodCommand()]
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import string
 import re
-import command
+
 from spellchecker import SpellChecker
+
+import command
+
 
 def read_sentence():
     print("@ ", end="")
@@ -14,8 +16,10 @@ def read_sentence():
         spell_command.extend(spell.candidates(i))
     return (command, raw_command, spell_command)
 
+
 sentence, raw_command, spell_command = read_sentence()
 while not 'exit' in sentence:
     pertaining_commands = [i for i in command.available_commands if i.pertains(sentence, raw_command, spell_command)]
-    len(pertaining_commands) > 0 and pertaining_commands[0].run(sentence, raw_command, spell_command) # Make this more sophisticated
+    len(pertaining_commands) > 0 and pertaining_commands[0].run(sentence, raw_command,
+                                                                spell_command)  # Make this more sophisticated
     sentence, raw_command, spell_command = read_sentence();
